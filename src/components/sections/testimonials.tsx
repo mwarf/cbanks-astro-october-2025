7fa17695e775eb7413a961e1905d4ee154ec3022
Yassine Zaanouni <yassinezaanouni@gmail.com>
2025-03-10 23:18:27 +0100

    feat: add login signup pages and fix testimonial

--- src/components/sections/testimonials.tsx
+++ src/components/sections/testimonials.tsx
@@ -1,76 +1,73 @@
+import { ArrowRight } from "lucide-react";
 
-
-import { ArrowRight } from 'lucide-react';
-
-import { DashedLine } from '../elements/dashed-line';
-
-import { Button } from '@/components/ui/button';
-import { Card, CardContent } from '@/components/ui/card';
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
 import {
   Carousel,
   CarouselContent,
   CarouselItem,
   CarouselNext,
   CarouselPrevious,
-} from '@/components/ui/carousel';
-import { cn } from '@/lib/utils';
+} from "@/components/ui/carousel";
+import { cn } from "@/lib/utils";
+import { DashedLine } from "../elements/dashed-line";
 
 const items = [
   {
     quote: "We're misusing Mainline as a CRM and it still works!",
-    author: 'Amy Chase',
-    role: 'PM',
-    company: 'Mercury Finance',
-    image: '/testimonials/amy-chase.webp',
+    author: "Amy Chase",
+    role: "PM",
+    company: "Mercury Finance",
+    image: "/testimonials/amy-chase.webp",
   },
   {
-    quote: 'I was able to replace 80% of my team with Mainline bots.',
-    author: 'Jonas Kotara',
-    role: 'Lead Engineer',
-    company: 'Mercury Finance',
-    image: '/testimonials/jonas-kotara.webp',
+    quote: "I was able to replace 80% of my team with Mainline bots.",
+    author: "Jonas Kotara",
+    role: "Lead Engineer",
+    company: "Mercury Finance",
+    image: "/testimonials/jonas-kotara.webp",
   },
   {
-    quote: 'Founder Mode is hard enough without having a really nice PM app.',
-    author: 'Kevin Yam',
-    role: 'Founder',
-    company: 'Mercury Finance',
-    image: '/testimonials/kevin-yam.webp',
+    quote: "Founder Mode is hard enough without having a really nice PM app.",
+    author: "Kevin Yam",
+    role: "Founder",
+    company: "Mercury Finance",
+    image: "/testimonials/kevin-yam.webp",
   },
   {
-    quote: 'I can use the tool as a substitute from my PM.',
-    author: 'Kundo Marta',
-    role: 'Founder',
-    company: 'Mercury Finance',
-    image: '/testimonials/kundo-marta.webp',
+    quote: "I can use the tool as a substitute from my PM.",
+    author: "Kundo Marta",
+    role: "Founder",
+    company: "Mercury Finance",
+    image: "/testimonials/kundo-marta.webp",
   },
   {
     quote: "We're misusing Mainline as a CRM and it still works!",
-    author: 'Amy Chase',
-    role: 'PM',
-    company: 'Mercury Finance',
-    image: '/testimonials/amy-chase.webp',
+    author: "Amy Chase",
+    role: "PM",
+    company: "Mercury Finance",
+    image: "/testimonials/amy-chase.webp",
   },
   {
-    quote: 'I was able to replace 80% of my team with Mainline bots.',
-    author: 'Jonas Kotara',
-    role: 'Lead Engineer',
-    company: 'Mercury Finance',
-    image: '/testimonials/jonas-kotara.webp',
+    quote: "I was able to replace 80% of my team with Mainline bots.",
+    author: "Jonas Kotara",
+    role: "Lead Engineer",
+    company: "Mercury Finance",
+    image: "/testimonials/jonas-kotara.webp",
   },
   {
-    quote: 'Founder Mode is hard enough without having a really nice PM app.',
-    author: 'Kevin Yam',
-    role: 'Founder',
-    company: 'Mercury Finance',
-    image: '/testimonials/kevin-yam.webp',
+    quote: "Founder Mode is hard enough without having a really nice PM app.",
+    author: "Kevin Yam",
+    role: "Founder",
+    company: "Mercury Finance",
+    image: "/testimonials/kevin-yam.webp",
   },
   {
-    quote: 'I can use the tool as a substitute from my PM.',
-    author: 'Kundo Marta',
-    role: 'Founder',
-    company: 'Mercury Finance',
-    image: '/testimonials/kundo-marta.webp',
+    quote: "I can use the tool as a substitute from my PM.",
+    author: "Kundo Marta",
+    role: "Founder",
+    company: "Mercury Finance",
+    image: "/testimonials/kundo-marta.webp",
   },
 ];
 
@@ -83,26 +80,26 @@ export const Testimonials = ({
 }) => {
   return (
     <>
-      <section className={cn('py-28 lg:py-32', className)}>
+      <section className={cn("py-32", className)}>
         <div className="container">
           <div className="space-y-4">
             <h2 className="text-2xl font-semibold tracking-tight md:text-4xl lg:text-5xl">
               Trusted by product builders
             </h2>
-            <p className="text-muted-foreground max-w-md leading-snug font-medium">
+            <p className="max-w-md font-medium leading-snug text-muted-foreground">
               Mainline is built on the habits that make the best product teams
               successful: staying focused, moving quickly, and always aiming for
               high-quality work.
             </p>
-            <Button variant="outline" className="shadow-md">
+            <Button variant="outline" className="">
               Read our Customer Stories <ArrowRight className="size-4" />
             </Button>
           </div>
 
-          <div className="relative mt-8 -mr-[max(3rem,calc((100vw-80rem)/2+3rem))] md:mt-12 lg:mt-20">
+          <div className="relative mt-8 md:mt-12 lg:mt-20">
             <Carousel
               opts={{
-                align: 'start',
+                align: "start",
                 loop: true,
               }}
               className="w-full"
@@ -111,28 +108,25 @@ export const Testimonials = ({
                 {items.map((testimonial, index) => (
                   <CarouselItem
                     key={index}
-                    className="xl:basis-1/3.5 grow basis-4/5 sm:basis-3/5 md:basis-2/5 lg:basis-[28%] 2xl:basis-[24%]"
+                    className="grow basis-4/5 md:basis-3/5 lg:basis-[28%] xl:basis-1/4"
                   >
-                    <Card className="bg-muted h-full overflow-hidden border-none">
+                    <Card className="h-full overflow-hidden border-none bg-muted">
                       <CardContent className="flex h-full flex-col p-0">
-                        <div className="relative h-[288px] lg:h-[328px]">
-                          <img
-                            src={testimonial.image}
-                            alt={testimonial.author}
-                            fill
-                            className="object-cover object-top"
-                          />
-                        </div>
+                        <img
+                          src={testimonial.image}
+                          alt={testimonial.author}
+                          className="h-[288px] object-cover object-top lg:h-[328px]"
+                        />
                         <div className="flex flex-1 flex-col justify-between gap-10 p-6">
-                          <blockquote className="text-primary font-sans text-lg leading-none! font-medium md:text-xl lg:text-2xl">
+                          <blockquote className="leading-none! text-lg font-medium text-primary md:text-xl">
                             {testimonial.quote}
                           </blockquote>
                           <div className="space-y-0.5">
-                            <div className="font-semibold">
-                              {testimonial.author}, {testimonial.role}
+                            <div className="text-sm font-semibold">
+                              {testimonial.author}
                             </div>
-                            <div className="text-muted-foreground text-sm">
-                              {testimonial.company}
+                            <div className="text-xs text-muted-foreground">
+                              {testimonial.role}, {testimonial.company}
                             </div>
                           </div>
                         </div>
@@ -142,8 +136,8 @@ export const Testimonials = ({
                 ))}
               </CarouselContent>
               <div className="mt-8 flex gap-3">
-                <CarouselPrevious className="bg-muted hover:bg-muted/80 static size-14.5 translate-x-0 translate-y-0 transition-colors [&>svg]:size-6 lg:[&>svg]:size-8" />
-                <CarouselNext className="bg-muted hover:bg-muted/80 static size-14.5 translate-x-0 translate-y-0 transition-colors [&>svg]:size-6 lg:[&>svg]:size-8" />
+                <CarouselPrevious className="static size-10 translate-x-0 translate-y-0" />
+                <CarouselNext className="static size-10 translate-x-0 translate-y-0" />
               </div>
             </Carousel>
           </div>
@@ -151,7 +145,7 @@ export const Testimonials = ({
       </section>
       <DashedLine
         orientation="horizontal"
-        className={cn('mx-auto max-w-[80%]', dashedLineClassName)}
+        className={cn("mx-auto max-w-[80%]", dashedLineClassName)}
       />
     </>
   );
